70a77883dee18806ddc6c66b321b948f596d6af8
Arlab0011 <36270898+Arlab0011@users.noreply.github.com>
2020-07-22 21:10:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ import { Card, CardGrid, Title, SimpleCell } from '@vkontakte/vkui'
 import { transitions, positions, Provider as AlertProvider } from 'react-alert'
 import AlertTemplate from 'react-alert-template-basic'
 import { useAlert } from 'react-alert'
-
 import socketIOClient from "socket.io-client";
 
 const blueBackground = {
